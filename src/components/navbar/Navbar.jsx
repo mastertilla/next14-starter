8b7af4f1fa633c8400efba9b7e71888a8bf5672a
Daniel Montilla <danielmontillanavas@gmail.com>
2024-10-13 17:35:36 +0400

    feat(): Navbar + links to routes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,13 @@
+import Links from '@/components/navbar/links/Links'
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      <div>Logo</div>
+      <div>
+        <Links />
+      </div>
+    </div>
   )
 }
 
