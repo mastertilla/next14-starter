889aedb86fbb72037889b8c45ab8bfb4756b68c7
Daniel Montilla <danielmontillanavas@gmail.com>
2024-10-19 12:14:20 +0400

    feat(): Navbar + styles

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,17 @@
 import Links from '@/components/navbar/links/Links'
+import styles from "./navbar.module.css"
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 const Navbar = () => {
   return (
-    <div>
-      <div>Logo</div>
-      <div>
-        <Links />
+    <div className={inter.className}>
+      <div  className={styles.container}>
+        <div className={styles.logo}>Logo</div>
+          <div>
+            <Links />
+          </div>
       </div>
     </div>
   )
